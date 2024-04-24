4fe085773a5d0964869c32f70ae2bc148b837a74
SkylarGoodwin <JamesSkylarGoodwin@gmail.com>
2024-04-24 16:01:34 -0500

    added semicolons

--- app.js
+++ app.js
@@ -4,25 +4,25 @@ console.log(
 );
 
 // Exercise 1
-let lastName
-var age
+let lastName;
+var age;
 
 // Exercise 2
-const firstName = "James"
-lastName = "Goodwin"
-age = 25
+const firstName = "James";
+lastName = "Goodwin";
+age = 25;
 // Exercise 3
-let language = "JavaScript"
-let createdYear = 1995
-let isCaseSensitive = true
-let price = 19.99
-let isOnSale = false
-let salePercentage = 15
-let stock = 0
-let inSock = false
-let selectSize = "M"
-let title = "Name of the Wind"
-let author = "Patrick Rothfuss"
-let pageCount = 722
-let bookmark = 456
-let hasRead = true
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inSock = false;
+let selectSize = "M";
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
